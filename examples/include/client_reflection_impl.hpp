396e9ea4b9e9a351848e051e1de6dd90430d766d
drec357 <davrec@gmail.com>
2019-11-10 18:50:07 -0500

    Update client_reflection_impl.hpp

--- examples/include/client_reflection_impl.hpp
+++ examples/include/client_reflection_impl.hpp
@@ -1,5 +1,5 @@
 
 // CHANGE THIS LOCATION to the path to your real client_reflection_impl.hpp if necessary.
 // (Would probably be best to change this to an absolute path.)
-#include "../build/include/client_reflection_impl.hpp"
+#include "../../build/include/client_reflection_impl.hpp"
 
